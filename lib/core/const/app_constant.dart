4d513d38d8a68828e1e832ab0990ede672d11732
Mohamed Fawzy <mofawzy.com7@gmail.com>
2025-09-06 10:22:56 +0300

    initialize supabase

---
+++ lib/core/const/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  // initialize supabase
+  static const String supabaseURL = 'https://jxeplfuzjlqjgejexqqn.supabase.co';
+  static const String supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZXBsZnV6amxxamdlamV4cXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMjY4MDYsImV4cCI6MjA3MjcwMjgwNn0.HzKbXDZ8-zFdi2pycluxGe_EFIED1R80FP4i4b0vlIY';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:study_box/core/const/app_constant.dart';
 import 'package:study_box/study_box_app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: AppConstant.supabaseURL,
+    anonKey: AppConstant.supabaseKey,
+  );
+
   runApp(
     DevicePreview(
       enabled: false,
