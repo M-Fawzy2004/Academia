006db6039949f67cf6d33703c82e05d265f66c38
Mohamed Fawzy <mofawzy.com7@gmail.com>
2025-09-08 12:00:03 +0300

    feat(auth): setup Google & Apple sign-in configuration

--- lib/core/const/app_constant.dart
+++ lib/core/const/app_constant.dart
@@ -4,6 +4,14 @@ class AppConstant {
   static const String supabaseKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZXBsZnV6amxxamdlamV4cXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMjY4MDYsImV4cCI6MjA3MjcwMjgwNn0.HzKbXDZ8-zFdi2pycluxGe_EFIED1R80FP4i4b0vlIY';
 
+  // client ID Google
+  static String googleClientID =
+      '362884712086-bpsod09kjn2nrh5e93t9cq0ch35u7sjf.apps.googleusercontent.com';
+
+  // client ID Apple
+  static String appleClientID =
+      '362884712086-nrev0ugkqg181vhuudln6cagrg38gca2.apps.googleusercontent.com';
+
   // supabase tables
   // user auth tables
   static const String tableAuthUsers = 'user_profiles';
--- lib/feature/auth/presentation/view/widget/login_with_social.dart
+++ lib/feature/auth/presentation/view/widget/login_with_social.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:study_box/core/helper/spacing.dart';
 import 'package:study_box/core/helper/translate.dart';
 import 'package:study_box/core/utils/assets.dart';
+import 'package:study_box/feature/auth/presentation/manager/cubit/auth_cubit.dart';
 import 'package:study_box/feature/auth/presentation/view/widget/social_login_button.dart';
 
 class LoginWithSocial extends StatelessWidget {
@@ -14,13 +16,17 @@ class LoginWithSocial extends StatelessWidget {
         SocialLoginButton(
           text: context.tr.login_with_google,
           assetPath: Assets.imagesIconGoogle,
-          onTap: () {},
+          onTap: () {
+            context.read<AuthCubit>().signInWithGoogle();
+          },
         ),
         heightBox(15),
         SocialLoginButton(
           text: context.tr.login_with_apple,
           assetPath: Assets.imagesIconApple,
-          onTap: () {},
+          onTap: () {
+            context.read<AuthCubit>().signInWithApple();
+          },
         ),
       ],
     );
